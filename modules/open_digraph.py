9ba476caa5b4698d3a46e40c6d6c52e2c50ed8f3
PENELOUX Louis-Alexis <loualeOldPC@outlook.com>
2022-01-21 16:59:24 +0100

    jeSuisGentil

--- modules/open_digraph.py
+++ modules/open_digraph.py
@@ -22,6 +22,36 @@ class node:
             raise Exception("Les Noeuds sont egaux mais n'ont pas le meme dict enfants")
         return True
 
+    def get_id(self):
+        return self.id
+    
+    def get_label(self):
+        return self.label
+    
+    def get_parent_ids(self):
+        return self.parents
+
+    def get_children_ids(self):
+        return self.children
+
+    def set_id(self, i):
+        self.id = i
+
+    def set_label(self, label):
+        self.label = label
+
+    def set_parents_ids(self, parents):
+        self.parents = parents
+
+    def set_children_ids(self, children):
+        self.children = children
+
+    def add_child_id(self, child):
+        self.children.update({child.id:child})
+
+    def add_parent_id(self, parent):
+        self.children.update({parent.id:parent})
+
     def __str__(self):
          return self.label + " " + str(self.id) + " " + str(self.children) + " " + str(self.parents)
 
@@ -42,6 +72,36 @@ class open_digraph: # for open directed graph
         self.inputs = inputs
         self.outputs = outputs
         self.nodes = {node.id:node for node in nodes} # self.nodes: <int,node> dict
+
+    def get_input_ids(self):
+        return self.inputs
+    
+    def get_output_ids(self):
+        return self.outputs
+
+    def get_id_node_map(self):
+        return self.node
+
+    def get_nodes(self):
+        return self.nodes.values()  
+
+    def get_node_ids(self):
+        return self.nodes.keys()
+
+    def get_node_by_id(self, id):
+        return self.nodes[id]
+
+    def set_input_ids(self, inputs):
+        self.inputs = inputs
+
+    def add_input_id(self, input):
+        self.inputs.append(input)
+
+    def set_output_ids(self, outputs):
+        self.outputs = outputs
+
+    def add_output_id(self, output):
+        self.outputs.append(output)
 
     def __str__(self):
         res = ""
