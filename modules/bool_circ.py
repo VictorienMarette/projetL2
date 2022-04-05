42cb09dfca18a2b76095ecbed2abcec880fd97f0
victorien <victorienmarette@gmail.com>
2022-04-05 14:51:53 +0200

    yo

--- modules/bool_circ.py
+++ modules/bool_circ.py
@@ -1,6 +1,7 @@
 from tkinter.filedialog import Open
 from modules.matrice import *
 from modules.open_digraph import *
+import math
 import random
 import os
 
@@ -86,3 +87,78 @@ def parse_parentheses(*strings : str) -> bool_circ:
                     entree_unique = False
                     break
     return g
+
+def code_de_grey(n):
+    if n == 1:
+        return ["0","1"]
+    
+    l = code_de_grey(n-1)
+    newl = []
+
+    lm = len(l)
+    for k in range(lm):
+        newl.append("0"+l[k])
+    for k in range(lm):
+        newl.append("1"+l[lm-1-k])
+
+    return newl
+
+
+def K_map(s):
+    n = int(math.log(len(s), 2))
+    l1 = code_de_grey(int(n/2))
+    l2 = code_de_grey(n - int(n/2))
+
+    l = []
+
+    for x in range(len(l1)):
+        l.append([int(s[int(l1[x] + l2[y], 2)]) for y in range(len(l2))])
+
+    return l
+    
+def K_to_f_propositionel(K):
+    Xl = len(K)
+    o = int(math.log(Xl, 2))
+    Yl = len(K[0])
+    p = int(math.log(Xl, 2))
+
+    def sub_total(i1, i2,j1,j2):
+        tot = 0
+        tot2 = 0
+        for i in range(i1,i2+1):
+            for j in range(j1,j2+1):
+                if K[i%Xl][j%Yl] == 1:
+                    tot += 1
+                if K[i%Xl][j%Yl]:
+                    tot2 += 1
+                
+        return tot, tot2
+
+    n = 0
+    for x in range(Xl):
+        for y in range(Yl):
+            n += K[x][y]
+
+    l = []
+
+    while n > 0:
+        max = 0
+        s_l = [0,0,0,0]
+
+        for i in range(1,o+1):
+            for j in range(1,p+1):
+                for x in range(Xl):
+                    for y in range(Yl):
+                        s1,s2 = sub_total(x,x+2**i-1, y, y+2**j-1)
+                        if s1 > max and s2 == 2**(i+j):
+                            s_l = [x,(x+2**i-1)%Xl, y, (y+2**j-1)%Yl]
+                            max = s1
+        
+        l.append(s_l.copy())
+        for i in range(s_l[0],s_l[1]+1):
+            for j in range(s_l[2],s_l[3]+1):
+                K[i%Xl][j%Yl] = -1
+        n += -max
+        print(n)
+
+    return l
--- vicworksheet.py
+++ vicworksheet.py
@@ -5,7 +5,7 @@ from modules.matrice import *
 from modules.bool_circ import *
 import inspect
 
-d = open_digraph([0, 1], [4], [
+"""d = open_digraph([0, 1], [4], [
                     node(0, 'i0', {}, {2:1,5:1}),
                     node(1, 'i1', {}, {2:1}),
                     node(2, 'n2', {0:1, 1:1}, {3:2}),
@@ -13,4 +13,21 @@ d = open_digraph([0, 1], [4], [
                     node(5, 'n4', {3:1,0:1}, {4:1}),
                     node(4, 'o4', {5: 1}, {})]
                     )
-d.display(verbose=True)
+#d.display(verbose=True)"""
+def sub_total(i1, i2,j1,j2, K):
+    Xl = len(K)
+    Yl = len(K[0])
+    tot = 0
+    tot2 = 0
+    for i in range(i1,i2+1):
+        for j in range(j1,j2+1):
+            if K[i%(Xl-1)][j%(Yl-1)] == 1:
+                tot += 1
+            if K[i%(Xl-1)][j%(Yl-1)]:
+                tot2 += 1
+            
+    return tot, tot2
+
+K = K_map("1110001000111111")
+sub_total(2, 2, 0, 1, K)
+#print(K_to_f_propositionel(K))
