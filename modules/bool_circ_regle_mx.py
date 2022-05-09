340ff1c05f0c87aeac2699f1adf49b5ef818f964
PENELOUX Louis-Alexis <loualeOldPC@outlook.com>
2022-05-09 17:51:18 +0200

    ca marche !

--- modules/bool_circ_regle_mx.py
+++ modules/bool_circ_regle_mx.py
@@ -28,9 +28,9 @@ class bool_circ_regle_mx(open_digraph):
             my_parent = self.get_node_by_id(idEntre)
             if my_parent.get_label() in ["0","1"]:
                 my_childrens = my_node.get_children_ids()
-                for ch in my_childrens:
-                    self.add_node(str((int(my_parent.get_label()) + 1)%2), {},{ch:1})
-                self.remove_node_by_id(id)
+                ch = my_childrens[0]
+                self.add_node(str((int(my_parent.get_label()) + 1)%2), {},{ch:1})
+                self.remove_node_by_id(idNon)
 
     def regle_porte_et(self, idEt, idEntre):
         # idEt pointe sur le noeud et
@@ -142,6 +142,8 @@ class bool_circ_regle_mx(open_digraph):
         pr = node_father.get_parent_ids()[0]
         self.add_edge(pr, id_child)
         self.remove_node_by_id(id_father)
+        self.remove_edge(id_child, ch)
+        print(self.idAffichage)
 
     def regle_involution_du_non(self, id_father, id_child):
         node_father = self.get_node_by_id(id_father)
@@ -155,18 +157,21 @@ class bool_circ_regle_mx(open_digraph):
         self.remove_node_by_id(id_child)
 
    #co feuille = noeud qui a des enfants mais pas de parents
-    def evaluate(self):
+    def evaluate(self, display = False, verbose = False):
         flag = True
         while flag:
+            flagDisplay = False
             flag = False
             for id in self.get_node_ids():
                 if(id in self.get_node_ids()):
                     # au cas ou le noeud n'a pas ete suppr de la liste mais suppr du graph
                     myNode = self.get_node_by_id(id)
                     if myNode.get_parent_ids() == []:
-                        # le noeud n'a pas de parent et n'est pas un input
+                        # le noeud n'a pas de parent
                         if myNode.get_children_ids() == []:
                             self.remove_node_by_id(id)
+                            if(display):
+                                self.display(verbose=verbose)
                             continue
                         if(myNode.get_label() not in ["0", "1"]):
                             self.regle_element_neutre(id)
@@ -179,19 +184,28 @@ class bool_circ_regle_mx(open_digraph):
                             if(str_op == ""):
                                 self.regle_copies(id_op, idEntre)
                                 flag = True
+                                if(display):
+                                    self.display(verbose=verbose)
                             elif(str_op == "&"):
                                 self.regle_porte_et(id_op, idEntre)
                                 flag = True
+                                if(display):
+                                    self.display(verbose=verbose)
                             elif(str_op == "|"):
                                 self.regle_porte_ou(id_op, idEntre)
                                 flag = True
+                                if(display):
+                                    self.display(verbose=verbose)
                             elif(str_op == "^"):
                                 self.regle_porte_ou_exclusif(id_op, idEntre)
                                 flag = True
+                                if(display):
+                                    self.display(verbose=verbose)
                             elif(str_op == "~"):
                                 self.regle_porte_non(id_op, idEntre)
                                 flag = True
-                            self.display(verbose=true)
+                                if(display):
+                                    self.display(verbose=verbose)
                     else:
                         
                         # le noeud a forcement des parents
@@ -203,6 +217,8 @@ class bool_circ_regle_mx(open_digraph):
                                 if(myChild.get_label() == "^"):
                                     self.regle_associativite_XOR(id, ch)
                                     flag = True
+                                    if(display):
+                                        self.display(verbose=verbose)
                             # bug
                             if(str_father == ""):
                                 for idn in myNode.get_children_ids():
@@ -211,9 +227,13 @@ class bool_circ_regle_mx(open_digraph):
                                         if n.get_label() == "":
                                             self.regle_associativite_copie(id, idn)
                                             flag = True
+                                            if(display):
+                                                self.display(verbose=verbose)
                                         if(n.get_label() == "^" and idn in myNode.children and myNode.children[idn] >= 2):
                                             self.regle_involution_XOR(id, n.get_id())
                                             flag = True
+                                            if(display):
+                                                self.display(verbose=verbose)
                             # endbug
                             if(str_father == "~"):
                                 ch = myNode.get_children_ids()[0]
@@ -222,13 +242,21 @@ class bool_circ_regle_mx(open_digraph):
                                 if(str_child == "^"):
                                     self.regle_non_a_travers_XOR(id, ch)
                                     flag = True
+                                    if(display):
+                                        self.display(verbose=verbose)
                                 elif(str_child == ""):
                                     self.regle_non_a_travers_copie(id, ch)
                                     flag = True
+                                    if(display):
+                                        self.display(verbose=verbose)
                                 elif(str_child == "~"):
                                     self.regle_involution_du_non(id, ch)
                                     flag = True
+                                    if(display):
+                                        self.display(verbose=verbose)
                         elif myNode.get_label() == "":
                             print(myNode.get_id())
                             self.regle_effacement(myNode.get_parent_ids()[0], id)
                             flag = True
+                            if(display):
+                                self.display(verbose=verbose)
